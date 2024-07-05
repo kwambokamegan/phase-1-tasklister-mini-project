05d19e5206e822b76a0f5cc7d33551a442754f9f
Megan <megan.nyakina@student.moringaschool.com>
2024-07-05 18:36:23 +0300

    adds tasklister mimi project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ const form = document.getElementById("create-task-form");
+ const taskList = document.getElementById("task-list");
+
+ form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const taskInput = document.getElementById("new-task-description")
+  const task = taskInput.value;
+  taskInput.value = "" ;
+
+  const taskElement = document.createElement("div");
+  taskElement.textContent = task;
+  taskList.appendChild(taskElement);
+ });
 });
